d85d70177b30ab1eb23e33d67148a3df83e0245f
Василий <v.babajkin@aerodisk.ru>
2026-01-16 15:46:55 +0400

    Решил задачу

--- find-the-index-of-the-first-occurrence-in-a-string/main.py
+++ find-the-index-of-the-first-occurrence-in-a-string/main.py
@@ -1,3 +1,15 @@
 class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.find(needle)
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+
+        return int(str(self.sqrt(x)).split('.')[0])
+
+
+    def sqrt(self, number):
+        eps = 1e-1
+        x = 1
+
+        while (abs(x * x - number) > eps):
+            x = (x + number / x) / 2
+        return x
